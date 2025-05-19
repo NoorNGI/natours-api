6e311c110f8a86e3d68458dc428bc4376b2b9f2d
NoorNGI <noor.ali@nextgeni.com>
2025-05-19 11:44:54 +0500

    connecting with mongo db

--- server/server.js
+++ server/server.js
@@ -1,7 +1,19 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import mongoose from "mongoose";
 import app from "../app.js";
+
+const DB = process.env.DATABASE_URL.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+console.log(DB, "db..");
+mongoose.connect(DB).then((con) => {
+  console.log(con, "---con");
+  console.log("DB connection successful");
+});
 
 const PORT = process.env.PORT || 3000;
 
